81701e97e73dbc990ab7cc8d5b72be1c411f853e
betomassoni <36550944+betomassoni@users.noreply.github.com>
2021-01-25 15:03:29 -0300

    Geração do projeto demo Spring em start.pring.io

---
+++ src/main/java/br/com/robertomassoni/carProtection/CarProtectionApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.robertomassoni.carProtection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarProtectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarProtectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/robertomassoni/carProtection/CarProtectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robertomassoni.carProtection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarProtectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
