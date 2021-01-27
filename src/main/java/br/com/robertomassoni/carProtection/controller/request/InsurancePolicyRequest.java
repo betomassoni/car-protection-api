832685d0e193a26a844018f0b311e95bb3da3997
betomassoni <36550944+betomassoni@users.noreply.github.com>
2021-01-27 16:24:06 -0300

    Criando regra para campos obrigatórios em apólice

--- src/main/java/br/com/robertomassoni/carProtection/controller/request/InsurancePolicyRequest.java
+++ src/main/java/br/com/robertomassoni/carProtection/controller/request/InsurancePolicyRequest.java
@@ -2,6 +2,7 @@ package br.com.robertomassoni.carProtection.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Date;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,10 +14,22 @@ import lombok.experimental.Accessors;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class InsurancePolicyRequest {
+    
+    @NotBlank(message = "Number is required.")
     private String number;
+    
+    @NotBlank(message = "Begin Date is required.")  
     private Date begin;
+    
+    @NotBlank(message = "End Date is required.")
     private Date end;
+    
+    @NotBlank(message = "Plate is required.")
     private String plate;
+    
+    @NotBlank(message = "Amount is required.")
     private Double amount;
+    
+    @NotBlank(message = "Client id is required.")
     private String clientId;
 }
